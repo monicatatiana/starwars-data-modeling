158bf91d7397cd4c8e737efac12bccb619122c60
monicatatiana <monictatiana@gmail.com>
2021-02-24 04:05:43 +0000

    pythom-apy-starwars-

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    type = Column(Boolean)
+    favorites_user = Column(Integer, ForeignKey(User.id))
+
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    eye_color = Column(String(250))
+    birthday = Column(Integer)
+    skin_color = Column(String(250))
+    height = Column(Integer)
+    gender = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    diameter = Column(Integer)
+    climate = Column(String(250))
+    terrain = Column(Integer)
+    population = Column(Integer)
+    surfaxe_Water = Column(Integer)
+
 
     def to_dict(self):
         return {}
